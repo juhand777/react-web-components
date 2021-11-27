9c48cf41f13ff71ece304a8f533a2b37f5c0d6f6
Johan Rosales <johan.rosales@tektonlabs.com>
2021-11-26 20:35:49 -0500

    Initial

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { useMyHook } from '@juhand777/base'
+import { useMyHook, TwilioLogo } from '@juhand777/base'
 
 const App = () => {
   const example = useMyHook()
   return (
     <div>
+      <TwilioLogo
+        height='240px'
+        width='520px'
+        customStyles={{ border: '1px solid black' }}
+      />
       {example}
     </div>
   )
